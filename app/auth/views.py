4344b5f660724c1f3492a6600ca2aac784901433
lhmqk <lamkhai2379@gmail.com>
2023-12-10 15:17:46 +0700

    init html templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, app
+from datetime import timedelta
 
 auth_blueprint = Blueprint('auth', __name__)
 
@@ -18,10 +19,16 @@ def login():
 def login_post():
     username = request.form.get('username')
     password = request.form.get('password')
+    keep_login = request.form.get('keep_login')
     # Validate credentials
     if users.get(username) == password:
         # Set the session as "logged_in"
         session['logged_in'] = True
+        # Set the session to not be permanent if the checkbox is not checked
+        session.permanent = 'remember' in request.form
+        if session.permanent:
+            # Set the duration of the session to be permanent (e.g., 30 days)
+            app.permanent_session_lifetime = timedelta(days=30)
         # Redirect to dashboard after successful login
         return redirect(url_for('dashboard.dashboard'))
     # Handle invalid credentials
